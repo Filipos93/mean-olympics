dabacbd669436f9cedac3d7cd44410297104f15a
Filip M <filipm93@gmail.com>
2017-05-23 20:49:08 +0200

    Add angular ui-router, list medals

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,67 @@
 import angular from 'angular'
-angular.module('olympics', [])
-.controller('sportsController', function($http){
-  $http.get('/sports').then((response)=>{
-    this.sports = response.data;
-  })
+import 'angular-ui-router'
+
+angular.module('olympics', ["ui.router"])
+
+.config(($stateProvider, $urlRouterProvider)=>{
+	
+	$urlRouterProvider.otherwise('/sports')
+	
+	$stateProvider.state('sports', {
+		
+		url: '/sports',
+		
+		templateUrl: 'sports/sports-nav.html',
+
+		resolve: {
+			sportsService: function($http){
+				return $http.get('/sports');
+			}
+		},
+		
+		controller: function(sportsService) {
+			this.sports = sportsService.data;
+		},
+
+		controllerAs: 'sportsCtrl'
+	
+	})
+
+	$stateProvider.state('sports.medals',{
+		
+		url: '/:sportName',
+		
+		templateUrl: 'sports/sports-medals.html',
+
+		resolve: {
+			sportService: function($q){
+				return $q((resolve, reject)=>{
+					let sport = {
+						"name" : "Cycling",
+						"goldMedals": [{
+							"division": "Men's Sprint",
+							"country": "UK",
+							"year" : 2017
+						},
+						{
+							"division": "Women's sprint",
+							"country": "Poland",
+							"year" : 2017
+						}]
+
+					};
+					resolve({data: sport});
+				});
+			}
+		},
+
+		controller: function(sportService){
+			this.sport = sportService.data;
+		},
+
+		controllerAs: 'sportCtrl'
+	
+	})
+
 })
+
