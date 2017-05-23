cbd712a3b8e6ccc635d71cd0ceab6e87d9a42fbf
Filip M <filipm93@gmail.com>
2017-05-23 18:26:38 +0200

    add mongo connection

--- server/app.js
+++ server/app.js
@@ -2,11 +2,18 @@
 
 let express = require('express');
 let app = express();
+let mongoUtil = require('./mongoUtil');
+
+mongoUtil.connect();
 
 app.use(express.static(__dirname + '/../client'));
 
 app.get('/sports', (request, response) => {
-	response.json(['Weightlifting', 'Cycling']);
+	let sports = mongoUtil.sports();
+	sports.find().toArray((err, docs)=>{
+		let sportsNames = docs.map((sports)=> sports.name);
+		response.json(sportsNames);
+	})
 })
 
 app.listen(8181, ()=> { console.log('listening on 8181'); });
