344c1ab9916a685cab41b3be644716b60854b43b
Filip M <filipm93@gmail.com>
2017-05-25 18:14:23 +0200

    deliver medals data from mongodb

--- server/app.js
+++ server/app.js
@@ -1,39 +1,40 @@
 "use strict";
 
-let express = require('express');
+let express = require("express");
 let app = express();
-let mongoUtil = require('./mongoUtil');
 
+let mongoUtil = require('./mongoUtil');
 mongoUtil.connect();
 
-app.use(express.static(__dirname + '/../client'));
+app.use( express.static(__dirname + "/../client") );
 
-app.get('/sports', (request, response) => {
-	let sports = mongoUtil.sports();
-	sports.find().toArray((err, docs)=>{
-		let sportsNames = docs.map((sports)=> sports.name);
-		response.json(sportsNames);
-	})
+app.get("/sports", (request, response) => {
+  let sports = mongoUtil.sports();
+  sports.find().toArray((err,docs) => {
+    if(err) {
+      response.sendStatus(400);
+    }
+    console.log(JSON.stringify(docs));
+    let sportNames = docs.map((sport) => sport.name);
+    response.json( sportNames );
+  });
 });
 
-app.get('/sports/:name', (request, response) => {
-	let sportName = request.params.name;
-	console.log('sport name: ', sportName);
-	let sport = {
-		"name" : "sdasdasd",
-		"goldMedals": [{
-			"division": "Men's Sprint",
-			"country": "UK",
-			"year" : 2017
-		},
-		{
-			"division": "Women's sprint",
-			"country": "Poland",
-			"year" : 3017
-		}]
+app.get("/sports/:name", (request, response) => {
+  let sportName = request.params.name;
 
-	};
-	response.json(sport);
-})
+  let sports = mongoUtil.sports();
+  sports.find({name: sportName}).limit(1).next((err,doc) => {
+    if(err) {
+      response.sendStatus(400);
+    }
+    console.log( "Sport doc: ", doc );
+    response.json(doc);
+  });
 
-app.listen(8181, ()=> { console.log('listening on 8181'); });
+});
+
+
+
+
+app.listen(8181, () => console.log( "Listening on 8181" ));
